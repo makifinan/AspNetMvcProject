152e06355435cae8e11d4baaf8eeb3d243d70787
makifinan <eposta@adresiniz.comm.akifinan@outlook.com.tr>
2022-01-27 01:04:48 +0300

    Added EntityFramework

---
+++ AspNetMvcProject/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+
+        public ActionResult AddCategory(Category category)
+        {
+            //categoryManager.Add(category);
+            return RedirectToAction("GetAll");
+        }
+    }
+}
